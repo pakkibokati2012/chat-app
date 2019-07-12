e1393d19ee675e5a19955fd8c08db8dbf565e62a
prakash <prakash.bokati@seelogic.co.uk>
2019-07-12 15:24:35 +0545

    getting used to socket.io

---
+++ public/js/chat.js
@@ -0,0 +1,12 @@
+const socket = io();
+
+socket.on('message', message => {
+  console.log(message);
+});
+
+const form = document.getElementById('message-form');
+form.onsubmit = function(e) {
+  e.preventDefault();
+  const message = document.getElementById('message').value;
+  socket.emit('sendMessage', message);
+};
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+io.on('connection', socket => {
+  socket.emit('message', 'Welcome!');
+  socket.on('sendMessage', message => {
+    console.log(message);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
